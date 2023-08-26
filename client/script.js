39363e845890f0ac5af9d1246c7d278a37efa8e7
proXDhiya <prox.dhiya@gmail.com>
2023-08-26 19:39:15 +0100

    Add client

---
+++ client/script.js
@@ -0,0 +1,21 @@
+const adviceId = document.getElementById('advice-number');
+const adviceText = document.getElementById('advice');
+const btn = document.getElementById('button');
+
+btn.addEventListener('click', async () => {
+    try {
+        // fetch data from http://localhost:3000/api/advices/random
+        const response = await fetch(
+            'http://localhost:3000/api/advices/random'
+        );
+
+        // convert response to json
+        const data = await response.json();
+
+        // update the dom
+        adviceId.children[0].innerHTML = data.randomAdvice.id;
+        adviceText.innerHTML = data.randomAdvice.str;
+    } catch (err) {
+        alert(err.message);
+    }
+});
